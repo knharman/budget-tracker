eb71b56cd362322bfc66c9d2cd77044e259a1a4d
Kellie Harman <knharman@gmail.com>
2022-03-27 12:59:41 -0700

    service worker intercepting fetch requests

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,18 +3,18 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./css/styles.css",
-    "./icons/icon-72x72.png",
-    './icons/icon-96x96.png',
-    './icons/icon-128x128.png',
-    './icons/icon-144x144.png',
-    './icons/icon-152x152.png',
-    './icons/icon-192x192.png',
-    './icons/icon-384x384.png',
-    './icons/icon-512x512.png',
-    './js/idb.js',
-    './js/index.js'
+    "/index.html",
+    "/css/styles.css",
+    "/icons/icon-72x72.png",
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png',
+    '/js/idb.js',
+    '/js/index.js'
 ];
 
 
@@ -34,7 +34,6 @@ self.addEventListener('activate', function (e) {
                 return key.indexOf(APP_PREFIX);
             });
             cacheKeeplist.push(CACHE_NAME);
-
             return Promise.all(
                 keyList.map(function (key, i) {
                     if (cacheKeeplist.indexOf(key) === -1) {
